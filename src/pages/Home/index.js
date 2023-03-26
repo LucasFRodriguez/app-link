3f5aa0b07569be0d1adf59f07a3ecfa72e611d69
unknown <lucasfrz26@gmail.com>
2023-03-26 13:58:52 -0300

    organizando rotas e criando o HTML

---
+++ src/pages/Home/index.js
@@ -0,0 +1,27 @@
+import {FiLink} from 'react-icons/fi';
+
+
+export default function Home(){
+    return(
+      <div className="container-home">
+        <div className="logo">
+
+          <h1>Incurtador de Link!</h1>
+          <span>Cole seu link para encurtar!</span>
+        </div>
+
+        <div className="area-input">
+          <div>
+            <FiLink size={24} color="#FFF" />
+            <input placeholder="Cole seu link aqui!"  />
+          </div>
+
+          <button>Encurtar Link</button>
+
+        </div>
+
+
+
+      </div>
+    )
+  }
---
+++ src/pages/Links/index.js
@@ -0,0 +1,8 @@
+
+export default function Links(){
+    return(
+      <div>
+        <h1>Pagina meus Link</h1>
+      </div>
+    )
+  }
